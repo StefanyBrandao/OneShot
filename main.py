faf482d28a9799b547b288457facc7e54dcfe76c
StefanyBrandao <stefany_drv@hotmail.com>
2024-12-08 09:24:13 -0300

    Adding classes generated from StarUML

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import pygame
 
 print('Setup Start')
 pygame.init()
-window=pygame.display.set_mode(
-    size=(800,500)
+window = pygame.display.set_mode(
+size=(800, 500)
 )
 print('Setup End')
 
 print('Loop Start')
 while True:
-    #Check for all events
+    # Check for all events
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            pygame.quit() #close window
-            quit() #end game
+        if event.type == pygame.QUIT:
+            print('Finalizando...')
+            pygame.quit()  # close window
