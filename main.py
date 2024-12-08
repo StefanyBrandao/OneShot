dfac2191f827ce9e9cb2b1de3c18b3cd25952498
StefanyBrandao <stefany_drv@hotmail.com>
2024-12-08 08:37:12 -0300

    Ok

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+window=pygame.display.set_mode(
+    size=(800,500)
+)
+print('Setup End')
+
+print('Loop Start')
+while True:
+    #Check for all events
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit() #close window
+            quit() #end game
